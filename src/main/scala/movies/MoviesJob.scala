2b060e78305ac7b4a59602ca685664baab978081
afcrowther <afcrowther@icloud.com>
2019-09-08 15:36:33 +0100

    Logic for credits in top 20 done, now to add tests

--- src/main/scala/movies/MoviesJob.scala
+++ src/main/scala/movies/MoviesJob.scala
@@ -12,33 +12,75 @@ import scala.util.Try
 
 object MoviesJob {
 
-  final case class InputDataFrames(ratingsDf: DataFrame)
+  final case class InputDataFrames(ratingsDf: DataFrame, titlesDf: DataFrame, crewsDf: DataFrame,
+                                   principalsDf: DataFrame, namesDf: DataFrame)
 
-  val ratingsSchema = StructType(Array(
-    StructField("movieId", StringType),
-    StructField("averageRating", DoubleType),
-    StructField("numberOfVotes", LongType)
-  ))
-
-  def runJob(ratingsFileLocation: String)(implicit sparkSession: SparkSession, reader: Reader, writer: Writer): Try[Unit] = {
+  def runJob(ratingsFileLocation: String, titlesFileLocation: String, crewsFileLocation: String,
+             principalsFileLocation: String, namesFileLocation: String)
+            (implicit sparkSession: SparkSession, reader: Reader, writer: Writer): Try[Unit] = {
 
     import sparkSession.sqlContext.implicits._
 
     val inputDataFrames = for {
-      ratingsDf ← reader.readFileToDataFrame(ratingsFileLocation, ratingsSchema)
-    } yield InputDataFrames(ratingsDf)
+      ratingsDf    ← reader.readFileToDataFrame(ratingsFileLocation, Schemas.ratingsSchema)
+      titlesDf     ← reader.readFileToDataFrame(titlesFileLocation, Schemas.titlesSchema)
+      crewsDf      ← reader.readFileToDataFrame(crewsFileLocation, Schemas.crewSchema)
+      principalsDf ← reader.readFileToDataFrame(principalsFileLocation, Schemas.principalsSchema)
+      namesDf      ← reader.readFileToDataFrame(namesFileLocation, Schemas.namesSchema)
+    } yield InputDataFrames(ratingsDf, titlesDf, crewsDf, principalsDf, namesDf)
 
-    inputDataFrames.map { dataFrames ⇒
+    inputDataFrames.flatMap { dataFrames ⇒
       // persist and repartition as using more than once
-      val persistedRatings = dataFrames.ratingsDf.repartition(200).persist(StorageLevel.MEMORY_AND_DISK_2)
+      val persistedMovieRatings = filterNonMovies(dataFrames.titlesDf.repartition(200), dataFrames.ratingsDf)
       // assumes that the ratings file is not empty
       val averageNumberOfVotesBroadcast = sparkSession.sparkContext.broadcast(
-        persistedRatings.select(avg('numberOfVotes).as("averageNumberOfVotes")).first().getDouble(0))
-      val top20MoviesByRatings = deriveTop20MoviesOnRatingsRanking(persistedRatings, averageNumberOfVotesBroadcast)
+        persistedMovieRatings.select(avg('numberOfVotes).as("averageNumberOfVotes")).first().getDouble(0))
+      // persist as using multiple times going forward
+      val top20MoviesByRatings = deriveTop20MoviesOnRatingsRanking(persistedMovieRatings, averageNumberOfVotesBroadcast)
+        .persist(StorageLevel.MEMORY_AND_DISK)
+      // write top 20 movies
       writer.writeDataFrame(top20MoviesByRatings)
-    }
 
+      // no longer needed
+      persistedMovieRatings.unpersist()
+      val principalsInTop20Df = top20MoviesByRatings.join(dataFrames.principalsDf, 'movieId)
+        .select('principalId.as("personId"))
+        .groupBy('personId)
+        .agg(count('personId).as("credits"))
+      // as we are exploding this dataframe (creating multiple rows from a single row in the input), it is worth us
+      // joining on the top 20 movies before we do the explode function, to reduce the amount of data we shuffle later
+      val crewInTop20Df = top20MoviesByRatings.join(dataFrames.crewsDf, 'movieId)
+        .select('directors, 'writers)
+        .persist(StorageLevel.MEMORY_AND_DISK)
+
+      val directorsInTop20Df = crewInTop20Df.select(splitStringUdf('directors).as("directors"))
+        .select(explode('directors).as("personId"))
+        .groupBy('personId)
+        .agg(count('personId).as("credits"))
+      val writersInTop20Df = crewInTop20Df.select('movieId, splitStringUdf('writers).as("writers"))
+        .select(explode('writers).as("personId"))
+        .groupBy('personId)
+        .agg(count('personId).as("credits"))
+
+      // assume every person id correlates with a name
+      val creditsInTop20Movies = principalsInTop20Df
+        .join(directorsInTop20Df, 'personId)
+        .join(writersInTop20Df, 'personId)
+        .groupBy('personId)
+        .agg(sum('credits))
+        .join(dataFrames.namesDf, 'personId)
+        .select('primaryName.as("name"), 'credits.as("credits_in_top_20_films"))
+      writer.writeDataFrame(creditsInTop20Movies)
+    }
   }
+
+  def filterNonMovies(titlesDf: DataFrame, ratingsDf: DataFrame): DataFrame = {
+    import ratingsDf.sqlContext.implicits._
+    titlesDf.filter('titleType === "movie")
+      .join(ratingsDf, 'titleId)
+      .select('titleId, 'averageRating, 'numberOfVotes, 'primaryTitle)
+  }
+
 
   /**
    * Will return the top twenty movies based on the ranking function:
@@ -49,7 +91,7 @@ object MoviesJob {
 
     import ratingsDf.sqlContext.implicits._
 
-    // add tie-breaker of numberOfVotes and movieId on ordering so that we remain deterministic (movie id assumed to be unique)
+    // add tie-breaker of numberOfVotes and movieId on ordering so that we remain deterministic (title id assumed to be unique)
     val window = Window.orderBy('rankingValue.desc, 'numberOfVotes.desc, 'movieId)
 
     // filter number of votes first to avoid aggregating unnecessary data
@@ -58,5 +100,16 @@ object MoviesJob {
         (ratingsDf("numberOfVotes") / averageNumberOfVotesBroadcast.value) * ratingsDf("averageRating"))
       // use row number rather than rank/ dense rank
       .withColumn("rank", row_number.over(window))
+  }
+
+  // need some custom logic to handle the '\\N
+  val splitStringUdf = udf { string: String => stringToList(string) }
+
+  // could be better
+  def stringToList(string: String): List[String] = string match {
+    case "" ⇒ Nil
+    case "\\N" ⇒ Nil
+      // assumes the input string is in the "a,b,c" pattern
+    case s ⇒ s.split(",").toList
   }
 }
--- src/main/scala/movies/package.scala
+++ src/main/scala/movies/package.scala
@@ -1,6 +1,49 @@
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.types.{ArrayType, BooleanType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
 
 package object movies {
 
+  object Schemas {
+    val ratingsSchema = StructType(Array(
+      StructField("titleId", StringType),
+      StructField("averageRating", DoubleType),
+      StructField("numberOfVotes", LongType)
+    ))
+
+    val titlesSchema = StructType(Array(
+      StructField("titleId", StringType),
+      StructField("titleType", StringType),
+      StructField("primaryTitle", StringType),
+      StructField("originalTitle", StringType),
+      StructField("isAdult", StringType),
+      StructField("startYear", StringType),
+      StructField("endYear", StringType),
+      StructField("runtimeMinutes", StringType),
+      StructField("genres", StringType)
+    ))
+
+    val crewSchema = StructType(Array(
+      StructField("movieId", StringType),
+      StructField("directorIds", ArrayType(StringType)),
+      StructField("writerIds", ArrayType(StringType))
+    ))
+
+    val principalsSchema = StructType(Array(
+      StructField("movieId", StringType),
+      StructField("ordering", IntegerType),
+      StructField("principalId", StringType),
+      StructField("category", StringType),
+      StructField("job", StringType),
+      StructField("characters", StringType)
+    ))
+
+    val namesSchema = StructType(Array(
+      StructField("personId", StringType),
+      StructField("primaryName", StringType),
+      StructField("birthYear", StringType),
+      StructField("deathYear", StringType),
+      StructField("primaryProfession", StringType),
+      StructField("knownForTitles", StringType)
+    ))
+  }
 }
